49777c64e9eaf790045585fb5f0fc58897611fdf
adamol <adamol1992@gmail.com>
2016-08-06 01:18:19 +0200

    Set up global config and autoloading

---
+++ core/init.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+$GLOBALS['config'] = [
+	'mysql' => [
+		'host' => '127.0.0.1',
+		'username' => 'root',
+		'password' => 'root',
+		'db' => 'oop-auth'
+	],
+	'remember' => [
+		'cookie_name' => 'hash',
+		'cookie_expiry' => 604800
+	],
+	'session' => [
+		'session_name' => 'user'
+	],
+];
+
+spl_autoload_register(function($class) {
+	require_once 'classes/' . $class . '.php';
+});
+
+require_once 'helpers/sanitize.php';
