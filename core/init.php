f642bf9f13913a8f0c577e4f4ac2630999bc58c0
adamol <adamol1992@gmail.com>
2016-08-06 01:42:06 +0200

    Setup singleton database class

--- core/init.php
+++ core/init.php
@@ -7,7 +7,7 @@ $GLOBALS['config'] = [
 		'host' => '127.0.0.1',
 		'username' => 'root',
 		'password' => 'root',
-		'db' => 'oop-auth'
+		'db' => 'oop_auth'
 	],
 	'remember' => [
 		'cookie_name' => 'hash',
