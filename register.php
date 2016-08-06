720c04e53bc35497e94355175f22754c3022fbca
adamol <adamol1992@gmail.com>
2016-08-06 13:10:54 +0200

    Set up register.php and Input class for form handling

--- register.php
+++ register.php
@@ -1,3 +1,30 @@
 <?php
 
 require_once 'core/init.php';
+
+if (Input::exists()) {
+	echo Input::get('username');
+}
+
+?>
+
+<form action="" method="post" autocomplete="off">
+	<div class="field">
+		<label for="username">username</label>
+		<input type="text" name="username" id="username" value="">
+	</div>
+	<div class="field">
+		<label for="password">password</label>
+		<input type="text" name="password" id="password" value="">
+	</div>
+	<div class="field">
+		<label for="password_confirm">password_confirm</label>
+		<input type="text" name="password_confirm" id="password_confirm" value="">
+	</div>
+	<div class="field">
+		<label for="name">name</label>
+		<input type="text" name="name" id="name" value="">
+	</div>
+
+	<input type="submit" value="Register">
+</form>
